999fee1a769830dd05fc47bb17ebafd24c37bf32
TzeSing <jjzzxx000@outlook.com>
2021-04-26 17:04:19 +0800

    feat: 禁止加载图片和css

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 # -*- coding: utf-8 -*-
+import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-option = Options()  # 实例化option对象
-option.add_argument("--headless")  # 给option对象添加无头参
+t0 = time.time()
+options = Options()  # 实例化option对象
+options.add_argument('--headless')  # 给option对象添加无头参
+prefs = {
+    'profile.managed_default_content_settings.images': 2,
+    'permissions.default.stylesheet': 2
+}
+options.add_experimental_option('prefs', prefs)
 
-driver = webdriver.Chrome(executable_path='./driver/chromedriver.exe', options=option)
+driver = webdriver.Chrome(executable_path='./driver/chromedriver.exe', options=options)
 driver.get('https://www.baidu.com')
 
 btn = driver.find_element_by_xpath('//*[@id="su"]')
 print(btn.get_attribute('value'))
 
 driver.close()
+
+print(f'耗时: {time.time() - t0}')
